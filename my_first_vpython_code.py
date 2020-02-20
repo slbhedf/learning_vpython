f3ed237b7bec07b4ecad02090aa993dfb519058f
slbhedf <57523792+slbhedf@users.noreply.github.com>
2020-02-20 16:35:50 +0900

    draw a simple sphere using VPython library.
    draw a simple sphere on a web browser like firefox, chrome, etc. using VPython library.
    https://www.vpython.org/

---
+++ my_first_vpython_code.py
@@ -0,0 +1,15 @@
+'''
+This Python file was created on 20 Feb 2020.
+
+Install vpython library using pip command below.
+
+$ pip install vpython
+
+1) Run this file "my_first_vpython_code.py"
+2) your default brower would be launched.
+3) you can see 3D graphic on your browser like firefox, chrome, etc.
+'''
+
+from vpython import*
+
+sphere(pos=vector(2,4,0), radius=5, color=color.white)
